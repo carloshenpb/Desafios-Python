3d6f3b50c0e3ea3c76f188836f15a10d68d051ea
Carlos Henrique Barbosa Pereira <carloshenpb@outlook.com>
2026-06-01 14:17:38 -0300

    Update manipulacaocsv.py

--- Manipulação-de-CSV/defs/manipulacao_csv/manipulacaocsv.py
+++ Manipulação-de-CSV/defs/manipulacao_csv/manipulacaocsv.py
@@ -1,6 +1,16 @@
 from os.path import exists, join
 
 def verificar_csv(arq_nome):
+    """
+Verifica se um arquivo CSV já existe em um diretório padrão do sistema.
+
+Argumentos:
+    arq_nome (str): O nome do arquivo a ser verificado (sem a extensão .csv).
+
+Retorna:
+    bool: Retorna False se o arquivo já existir (bloqueado) 
+          ou True se o arquivo não existir (liberado para criação).
+"""
     pasta_padrao = r'C:\Users\carlo\PycharmProjects\Desafios\Manipulação-de-CSV\arquivos'
     caminho_arquivo = join(pasta_padrao, f'{arq_nome}.csv')
     if exists(caminho_arquivo):
